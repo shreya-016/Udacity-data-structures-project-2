133805a1cb0fb0567a26c0e9a356afdf5137e254
shreya-016 <106731425+shreya-016@users.noreply.github.com>
2022-11-22 22:45:21 +0530

    Create problem_4.py

---
+++ problem_4.py
@@ -0,0 +1,48 @@
+# Active Directory
+class Group(object):
+    def __init__(self, _name):
+        self.name = _name
+        self.groups = []
+        self.users = []
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_groups(self):
+        return self.groups
+
+    def get_users(self):
+        return self.users
+
+    def get_name(self):
+        return self.name
+
+
+parent = Group("parent")
+child = Group("child")
+sub_child = Group("subchild")
+
+sub_child_user = "sub_child_user"
+sub_child.add_user(sub_child_user)
+
+child.add_group(sub_child)
+parent.add_group(child)
+
+
+def is_user_in_group(user, group):
+    if user in group.get_users():
+        return True
+    else:
+        for group in group.get_groups():
+            return is_user_in_group(user, group)
+            
+    return False
+
+print(is_user_in_group(sub_child_user, sub_child))  
+print(is_user_in_group(sub_child_user, child))
+print(is_user_in_group(sub_child_user, parent))
+print(is_user_in_group(parent, child))    
+    
